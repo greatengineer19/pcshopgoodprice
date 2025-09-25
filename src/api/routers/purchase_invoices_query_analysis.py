83f4e05bd98046e6bd66fd322523a6b2f90d3991
Juan Andrew <juandreww@outlook.com>
2025-09-25 21:55:57 +0700

    PI qa

--- src/api/routers/purchase_invoices_query_analysis.py
+++ src/api/routers/purchase_invoices_query_analysis.py
@@ -14,14 +14,18 @@ router = APIRouter(prefix='/api/purchase_invoices_query_analysis', tags=["Purcha
 @router.get("", response_model=PurchaseInvoicesQueryAnalysis, status_code=200)
 def analyze(db: Session = Depends(get_db)):
     try:
-        data_counts = [200000, 400000, 600000, 800000, 900000, 1000000, 1200000, 1400000, 1600000, 1800000, 2000000]
+        data_counts = [100000, 200000, 300000, 400000, 500000, 6000000, 700000, 800000, 900000, 1000000]
+        qtime1 = [0.63, 1.08, 1.34, 1.47, 1.65, 7.56, 7.13, 7.19, 7.32, 7.02]
+        qtime2 = [0.30, 0.30, 0.28, 0.27, 0.32, 0.28, 0.29, 0.28, 0.31, 0.30]
+        qtime3 = [60.0, 61.0, 61.0, 60.0, 61.0, 62.0, 60.0, 61.0, 62.0, 63.0]
+
         result = []
-        for data_count in data_counts:
+        for index, data_count in enumerate(data_counts):
             result.append({
                 'data_count': data_count,
-                'query_time1': random.randint(500, 1000),
-                'query_time2': random.randint(500, 1000),
-                'query_time3': random.randint(500, 1000),
+                'query_time1': qtime1[index],
+                'query_time2': qtime2[index],
+                'query_time3': qtime3[index],
             })
             
         return { 'message': 'Successfully analyzed 3 query performance', 'data': result }
