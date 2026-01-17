3772806ba94a0f013a260afb96d692716346a328
Juan Andrew <juandreww@outlook.com>
2026-01-17 08:11:41 +0700

    Removed frontend capabilities or RoR

--- rails-service/app/controllers/application_controller.rb
+++ rails-service/app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
-class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-
-  # Changes to the importmap will invalidate the etag for HTML responses
-  stale_when_importmap_changes
-
+class ApplicationController < ActionController::API
   rescue_from StandardError, NameError, with: :handle_standard_error
   rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
   rescue_from ActiveRecord::RecordInvalid, with: :handle_record_invalid
